5489257db09b4e1778ae1250316baaa58cb97b05
Warito-Kichiro <tonyandrew0308.gmail.com>
2024-07-02 01:24:30 +0900

    upload003

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -990,7 +990,7 @@ const Home = () => {
                           aria-hidden="true"
                         ></i>
                         <h3>Address</h3>
-                        <p>Tokyo, Mitaka-shi, Japan</p>
+                        <p>Street Voznesenska, building 100, flat 109, Reni, Ukraine</p>
                       </div>
                     </div>
                     <div className="col-md-3 col-sm-6 contact-colamn">
@@ -1001,14 +1001,14 @@ const Home = () => {
                         ></i>
                         <h3>Email</h3>
                         <a
-                          href="mailto:contact.tonyandrew0308@gmail.com"
+                          href="mailto:contact.mailto:wesemannkbf9@gmail.com"
                           style={{
                             color: "black",
                             textDecoration: "none",
                             fontSize: "17px",
                           }}
                         >
-                          tonyandrew0308@gmail.com
+                          mailto:wesemannkbf9@gmail.com
                         </a>
                       </div>
                     </div>
@@ -1020,14 +1020,14 @@ const Home = () => {
                         ></i>
                         <h3>Phone</h3>
                         <a
-                          href="tel:8372876775"
+                          href="tel:380989115943"
                           style={{
                             color: "black",
                             textDecoration: "none",
                             fontSize: "17px",
                           }}
                         >
-                          +1 702-501-7154
+                          +380 989115943
                         </a>
                       </div>
                     </div>
@@ -1038,29 +1038,11 @@ const Home = () => {
                           aria-hidden="true"
                         ></i>
                         <h3>Portfolio</h3>
-                        <p>https://portfolio-dev-bay-five.vercel.app/</p>
+                        <p>portfolio-dev-bay-five.vercel.app</p>
                       </div>
                     </div>
                   </div>
                 </div>
-                {/* <div class="container-fluid map-col">
-                  <div class="col-md-12 col-sm-12 map-col">
-                    <div class="google-maps">
-                      <div class="map-wrap">
-                        <iframe
-                          title="google-maps"
-                          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d471220.5630603985!2d88.0495328251319!3d22.675752087592436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39f882db4908f667%3A0x43e330e68f6c2cbc!2sKolkata%2C%20West%20Bengal!5e0!3m2!1sen!2sin!4v1653845709428!5m2!1sen!2sin"
-                          width="100%"
-                          height="450"
-                          style={{ border: "0" }}
-                          allowfullscreen=""
-                          loading="lazy"
-                          referrerpolicy="no-referrer-when-downgrade"
-                        ></iframe>
-                      </div>
-                    </div>
-                  </div>
-                </div> */}
               </div>
             </div>
           </div>
